d3b3915046efc8b8464df425dbfd8fdd2359df43
Thomas L. Kjeldsen <tlk@users.noreply.github.com>
2021-10-23 22:47:23 +0200

    Update beoplayremotegui.rb

--- Casks/beoplayremotegui.rb
+++ Casks/beoplayremotegui.rb
@@ -3,8 +3,8 @@ cask "beoplayremotegui" do
   sha256 "8db62d339837078599ccf3fe0a2effb35f00f9210795881858c5bd26f4971163"
 
   url "https://github.com/tlk/beoplay-macos-remote-gui/releases/download/v#{version}/BeoplayRemoteGUI.dmg"
-  name "Beoplay Remote (unofficial PoC)"
-  desc "Remote control B&O Beoplay loudspeakers from macOS"
+  name "Beoplay Remote GUI (unofficial PoC)"
+  desc "Remote control network enabled Bang & Olufsen loudspeakers from macOS"
   homepage "https://github.com/tlk/beoplay-macos-remote-gui"
 
   app "BeoplayRemoteGUI.app"
